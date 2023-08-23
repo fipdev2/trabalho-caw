0c4013f71d5d20f4edbaa7fe2b6b608301fec794
Filipe Magalhães <filipemagalha3s@gmail.com>
2023-08-23 17:18:06 -0300

    feature: react instalado com vite

---
+++ frontend/src/components/MainCard/index.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+
+interface aluno {
+    foto: string;
+    nome: string;
+    sobreMim: string;
+    tecnologias: string[];
+}
+
+function MainCard() {
+    const [aluno, setAluno] = useState<aluno>({
+        foto: '../../assets/eu hehe.jpeg', nome: 'Filipe',
+        sobreMim: 'Sempre gostei de games, e de séries, estudo programação desde 2022, gosto de tecnologias voltadas para o front-end e inteligência artificial',
+        tecnologias: ['ts', 'js', 'react', 'html', 'sql']
+    })
+
+
+    return (
+        <main className="mainCard">
+            <div>
+                <a>Filipe</a>
+                <a>Davi</a>
+                <a>Matheus</a>
+            </div>
+            <div>
+                <img className='alunoFoto' src={aluno.foto}/>
+                <div>
+                    <h1>Olá, eu sou {aluno.nome}</h1>
+                    <p>{aluno.sobreMim}</p>
+                    <div>
+                        <strong>Tecnologias</strong>
+
+                    </div>
+                </div>
+            </div>
+
+        </main>
+    );
+}
+
+export default MainCard;
---
+++ frontend/src/pages/HomePage/index.tsx
@@ -0,0 +1,12 @@
+import MainCard from '../../components/MainCard';
+import '../HomePage/index.css'
+
+function HomePage() {
+    return (
+        <div className="divContainer">
+            <MainCard />
+        </div>
+    );
+}
+
+export default HomePage;

ee30f385384488fdad9387cf43303ad3b6e14dd6
15Math <matheus.loureiro1215@gmail.com>
2023-08-23 17:45:36 -0300

    bugFix: import Img

--- frontend/src/components/MainCard/index.tsx
+++ frontend/src/components/MainCard/index.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-
+import FilipeImg from '../../assets/euhehe.jpeg';
 
 interface aluno {
-    foto: string;
+    foto:string;
     nome: string;
     sobreMim: string;
     tecnologias: string[];
@@ -10,7 +10,8 @@ interface aluno {
 
 function MainCard() {
     const [aluno, setAluno] = useState<aluno>({
-        foto: '../../assets/eu hehe.jpeg', nome: 'Filipe',
+        foto: FilipeImg,
+        nome: 'Filipe',
         sobreMim: 'Sempre gostei de games, e de séries, estudo programação desde 2022, gosto de tecnologias voltadas para o front-end e inteligência artificial',
         tecnologias: ['ts', 'js', 'react', 'html', 'sql']
     })
